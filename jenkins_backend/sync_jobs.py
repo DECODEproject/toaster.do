d245ab99395d59d8716cd19670190378590d564e
parazyd <parazyd@dyne.org>
2018-10-08 11:54:41 +0200

    fixup! Do not replace AT with @ when listing jobs.

--- jenkins_backend/sync_jobs.py
+++ jenkins_backend/sync_jobs.py
@@ -93,7 +93,7 @@ def list_jobs(account):
         if i.startswith(account.replace('@', 'AT')):
             parsedlist.append(i)
 
-    print('\n'.join(parsedlist)
+    print('\n'.join(parsedlist))
 
 
 def status_job(jobname):
